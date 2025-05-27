7aa004f5fab5093f9f126502349ff3ff12f46e46
Amin Albooyeh <amin.albooyeh@gmail.com>
2025-05-27 08:36:44 +0200

    Adding python file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Rock Paper Scissors Game
+This is a simple rock paper scissors game in Python.
+## How to play
+1. Choose your option (rock, paper, or scissors)
+2. The computer will choose its option randomly
+3. The winner will be determined based on the rules of the game
+4. The game will continue until one player wins three times
+## Rules of the game
+1. Rock beats scissors
+2. Scissors beats paper
+3. Paper beats rock 
+
--- src/main.py
+++ src/main.py
@@ -2,11 +2,12 @@ import random
 
 
 class RockPaperScissors:
+    """ This class represents the game of Rock-Paper-Scissors """
     def __init__(self):
         self.choices = ['rock', 'paper','scissors']
     
     def get_player_choice(self):
-        player_choice = input("Enter your choice (rock, paper, scissors): ")
+        player_choice: str = input("Enter your choice (rock, paper, scissors): ")
         if player_choice.lower() in self.choices:
             return player_choice.lower()
         
@@ -14,9 +15,18 @@ class RockPaperScissors:
         return self.get_player_choice()
     
     def get_computer_choice(self):
+        """ get computer choice randomly from the choices list: rock, paper, or scissors """
         return random.choice(self.choices)
 
-    def determine_winner(self, player_choice, computer_choice):
+    def determine_winner(self, player_choice: str, computer_choice: str) --> str:
+        """
+        Args:
+            player_choice (str): player's choice
+            computer_choice (str): computer's choice
+
+        Returns:
+            str: winner message
+        """
         if player_choice == computer_choice:
             print("It's a tie!")
             return 'It is a tie!'
@@ -25,10 +35,18 @@ class RockPaperScissors:
             or player_choice == 'paper' and computer_choice == 'rock'
             or player_choice =='scissors' and computer_choice == 'paper'):
             print("You win!")
+            return 'You win!'
         else:
             print("Computer wins!")
 
     def play(self):
+        """
+        - get user choice
+        - get computer choice
+        - determine winner
+        - print winner message
+        
+        """
         player_choice = self.get_player_choice()
         computer_choice = self.get_computer_choice()
         print(f"You chose {player_choice}, computer chose {computer_choice}.")
